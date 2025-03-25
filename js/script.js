7cbd0d3dcb097f4f9914eb13ac950969b09bc485
guihgalegodev <guigalego96@gmail.com>
2025-03-25 00:04:18 -0300

    add menu mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const btnMobile = document.getElementById("btn-mobile");
 
+function toogleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toogleMenu);
+btnMobile.addEventListener("touchstart", toogleMenu);
